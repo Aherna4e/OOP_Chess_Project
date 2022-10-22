8941f2c5f070f20c4181146a6d26d955a4a80b32
matth <matthew011001@gmail.com>
2022-10-21 22:13:58 -0500

    pieces

---
+++ src/Pieces/Piece.java
@@ -0,0 +1,24 @@
+package Pieces;
+
+public abstract class Piece {
+    private boolean white = false;
+    private boolean destroyed = false;
+    public abstract boolean viableMove(Square start, Square end, Board board);
+
+    public boolean isDestroyed() {
+        return this.destroyed;
+    }
+    public boolean isWhite() {
+        return this.white;
+    }
+    public void setWhite(boolean white) {
+        this.white = white;
+    }
+    public void setDestroyed(boolean destroyed) {
+        this.destroyed = destroyed;
+    }
+
+    public Piece(boolean white) {
+        this.setWhite(white);
+    }
+}
